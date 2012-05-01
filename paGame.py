07f06cc6eb964b51a411ce26c60caf6b44ee3135
PaquilloDubois <fjruizruano@gmail.com>
2012-05-01 19:15:24 +0100

    image backgroud

--- paGame.py
+++ paGame.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, sys
-import pygame
+import os, sys, pygame
 from pygame.locals import *
  
 if not pygame.font: print 'estilos y letras desactivadas!'
@@ -11,16 +10,31 @@ if not pygame.mixer: print 'sonidos desactivados!'
 WIDTH = 640
 HEIGHT = 480
 
+def carga_fondo (name, colorkey=None):
+	fullname = os.path.join("img", name)
+	try:
+		image = pygame.image.load(fullname)
+	except pygame.error, message:
+		print "No se puede cargar la imagen", name
+		raise SystemExit, message
+	image = image.convert()
+	return image, image.get_rect()
+
 def main():
 
-	window = pygame.display.set_mode((WIDTH, HEIGHT))
+	screen = pygame.display.set_mode((WIDTH, HEIGHT))
 	pygame.display.set_caption("¡Hola pyGame!")
+	background_image = carga_fondo("fondo.bmp")
+	screen.blit(background_image[0], (0, 0))
+	pygame.display.flip()	
 	while True:
 		for eventos in pygame.event.get():
 			if eventos.type == QUIT:
 				sys.exit(0)
+#		screen.blit(background_image, (0, 0))
+#		pygame.display.flip()	
 	return 0
 
 if __name__ == '__main__':
 	pygame.init()
-main()
+	main()
