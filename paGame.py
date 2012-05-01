60f5e40af9024efea5c25bd24f3596bae431eda6
PaquilloDubois <fjruizruano@gmail.com>
2012-05-01 18:40:36 +0100

    back screen

---
+++ paGame.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys
+import pygame
+from pygame.locals import *
+ 
+if not pygame.font: print 'estilos y letras desactivadas!'
+if not pygame.mixer: print 'sonidos desactivados!'
+
+WIDTH = 640
+HEIGHT = 480
+
+def main():
+
+	window = pygame.display.set_mode((WIDTH, HEIGHT))
+	pygame.display.set_caption("¡Hola pyGame!")
+	while True:
+		for eventos in pygame.event.get():
+			if eventos.type == QUIT:
+				sys.exit(0)
+	return 0
+
+if __name__ == '__main__':
+pygame.init()
+main()
